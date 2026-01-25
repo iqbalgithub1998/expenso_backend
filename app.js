e935ab73625140f23528df3f1b072ba8e3b2e068
Iqbal khan <iqbalkhanak45@gmail.com>
2026-01-26 00:06:17 +0530

    fix rooting of file

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 import express from "express";
 import cors from "cors";
-import authRoutes from "./routes/auth.routes.js";
-import expenseRoutes from "./routes/expense.routes.js";
-import friendRoutes from "./routes/friend.routes.js";
-import transactionRoutes from "./routes/transaction.routes.js";
-import analyticsRoutes from "./routes/analytics.routes.js";
+import authRoutes from "./src/routes/auth.routes.js";
+import expenseRoutes from "./src/routes/expense.routes.js";
+import friendRoutes from "./src/routes/friend.routes.js";
+import transactionRoutes from "./src/routes/transaction.routes.js";
+import analyticsRoutes from "./src/routes/analytics.routes.js";
 
 const app = express();
 
@@ -17,7 +17,7 @@ app.use("/api/friends", friendRoutes);
 app.use("/api/transactions", transactionRoutes);
 app.use("/api/analytics", analyticsRoutes);
 app.get("/health", (req, res) =>
-  res.json({ status: "ok", message: "Healthy", data: new Date().toString() })
+  res.json({ status: "ok", message: "Healthy", data: new Date().toString() }),
 );
 
 export default app;
--- server.js
+++ server.js
@@ -2,10 +2,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import app from "./app.js";
-import connectDB from "./config/db.js";
+import connectDB from "./src/config/db.js";
 
 connectDB();
 
 app.listen(process.env.PORT, () =>
-  console.log(`Server running on port ${process.env.PORT}`)
+  console.log(`Server running on port ${process.env.PORT}`),
 );
