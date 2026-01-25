e279ba773efc49a52542a1479a13403a3a7f08cf
Iqbal khan <iqbalkhanak45@gmail.com>
2026-01-26 00:37:51 +0530

    updating changes according to vercel

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 import dotenv from "dotenv";
 dotenv.config();
-
-import app from "./app.js";
-import connectDB from "./src/config/db.js";
 import { connect } from "mongoose";
+import express from "express";
+import cors from "cors";
+import authRoutes from "./src/routes/auth.routes.js";
+import expenseRoutes from "./src/routes/expense.routes.js";
+import friendRoutes from "./src/routes/friend.routes.js";
+import transactionRoutes from "./src/routes/transaction.routes.js";
+import analyticsRoutes from "./src/routes/analytics.routes.js";
 
+const app = express();
 let isConnected = false;
 
 async function connectToMongoDb() {
@@ -25,8 +30,24 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use(cors());
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.json({
+    message: "app running.",
+  });
+});
+app.use("/api/auth", authRoutes);
+app.use("/api/expenses", expenseRoutes);
+app.use("/api/friends", friendRoutes);
+app.use("/api/transactions", transactionRoutes);
+app.use("/api/analytics", analyticsRoutes);
+app.get("/health", (req, res) =>
+  res.json({ status: "ok", message: "Healthy", data: new Date().toString() }),
+);
+
 // app.listen(process.env.PORT, () =>
 //   console.log(`Server running on port ${process.env.PORT}`),
 // );
 
-module.exports = app;
+export default app;
