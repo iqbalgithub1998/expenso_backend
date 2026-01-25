f28e97c5782b2e4007784d24cf7cdc4061ce289b
Iqbal khan <iqbalkhanak45@gmail.com>
2026-01-26 00:27:58 +0530

    updating changes according to vercel

---
+++ server.js
@@ -0,0 +1,32 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import app from "./app.js";
+import connectDB from "./src/config/db.js";
+import { connect } from "mongoose";
+
+let isConnected = false;
+
+async function connectToMongoDb() {
+  try {
+    const conn = await connect(process.env.MONGODB_URI, {});
+    isConnected = true;
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`❌ MongoDB Connection Error: ${error.message}`);
+    process.exit(1);
+  }
+}
+
+app.use((req, res, next) => {
+  if (!isConnected) {
+    connectDB();
+  }
+  next();
+});
+
+// app.listen(process.env.PORT, () =>
+//   console.log(`Server running on port ${process.env.PORT}`),
+// );
+
+module.exports = app;
